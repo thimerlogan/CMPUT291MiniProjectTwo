722d7d8142a8d07df6be5e2580a2e360c02f9066
Scott Gebert <scott22.gebert@gmail.com>
2022-11-24 12:20:13 -0700

    WIP

--- src/venueSearch.py
+++ src/venueSearch.py
@@ -1,15 +1,134 @@
+
+
 import sys
 import json
 from pymongo import MongoClient
 import re
 
 
-def findVenues(n, collection):
-    # results = collection.find({"authors": "/" + keyword + "/"})
-    results = collection.find({'venue':{'$regex':"international conference on computer vision and graphics"}})
-    # results = collection.find({'venue': "international conference on computer vision and graphics"})
-    retVal = []
-    for venue in results:
-        retVal.append({"names": venue["authors"]})
-    return retVal
+def findVenues(n, collection, db, client):
+    # Grabs venues with the most articles
+    # results = collection.aggregate([
+    #     {'$group' :
+    #         {'_id': 
+    #             {'venue': "$venue"},
+    #         'name' : 
+    #             {'$addToSet': '$id' },
+    #         'venCount':
+    #             {'$sum' : 1}
+    #         }
+    #     }, 
+    #     {"$sort": 
+    #         { "venCount" : -1 }
+    #     }
+    # ])
+    
+    
+    
+    
+    client.admin.command({'setParameter': 1, 'internalQueryForceClassicEngine': True });
+    results = collection.aggregate([
+        {'$group' :
+            {'_id': 
+                {'venue': "$venue"},
+            'ids' : 
+                {'$addToSet': "$id"},          
+            'venCount':
+                {'$sum' : 1}
+            }
+        },
+        {
+            '$lookup': {
+                'from': 'dblp',
+                'localField': 'ids',
+                'foreignField': 'refernces',
+                'as': 'combined'
+            }
+        },
+        {"$sort": 
+            { "venCount" : 1 }
+        },
+        # {'$merge' : { 'into': { 'coll': "venue_merge" }, 'on': ['_id'],  'whenMatched': "replace", 'whenNotMatched': "insert" } }
+    ])
+    
+    
+    
 
+    
+    # results = collection.find({'references': {'$nin': ['']}})
+    
+                    # {'$addToSet': '$id' },
+    
+    # print(list(results[1:100]))
+
+    # results = db.collection['venue_merge'].find({})
+    # print(list(results))
+    for document in results:
+        if (document['_id']['venue'] == "Journal of Automata, Languages and Combinatorics"):
+            print(document)
+          
+    # print(list(results)[0:100])
+    
+    # count = 0
+    # for venue in results:
+    #     if (count == n):
+    #         continue
+        
+    #     if venue["_id"]["venue"] != "":
+    #         collection.find()
+            
+    #         print(venue["_id"]["venue"], "|", venue["venCount"], venue["name"])
+    #         count = count + 1
+    
+    
+    
+    # results = collection.distinct('venue')
+    # venueCount = {}
+    # for venue in results: 
+    #     if venue != '':
+    #         selectedVenues = (collection.find({"venue": venue}))
+           
+    #         # Get the ids of the venue and count references
+    #         ids = []
+    #         for venueselc in selectedVenues:
+    #             ids.append(venueselc["id"])
+                
+    #         print(ids)    
+    #         references = collection.find({"references" : {"$in": ids}})
+    #         # print(len(list(references)))
+            
+        
+    #         venueCount[venue] = {"articleCount": len(list(selectedVenues)), "refCount": len(list(references))}
+
+    # print(venueCount)
+    return results
+
+
+
+
+    # results = collection.aggregate([
+    #     {'$group' :
+    #         {'_id': 
+    #             {'venue': "$venue"},
+    #         'name' : 
+    #             {'$addToSet': '$id' },
+    #         'venCount':
+    #             {'$sum' : 1}
+    #         }
+    #     }, 
+    #     {"$sort": 
+    #         { "venCount" : -1 }
+    #     }
+    # ])
+    # # print(list(results)[0:100])
+    
+    # count = 0
+    # for venue in results:
+    #     if (count == n):
+    #         continue
+        
+    #     if venue["_id"]["venue"] != "":
+    #         collection.find()
+            
+    #         print(venue["_id"]["venue"], "|", venue["venCount"], venue["name"])
+    #         count = count + 1
